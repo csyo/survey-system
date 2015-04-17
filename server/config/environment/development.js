233f2fb158a184807208f5b53c7a0029a0597663
csyo <csyrick@icloud.com>
2015-04-17 23:39:17 +0800

    Init app

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://csyo:csyopass@ds061711.mongolab.com:61711/questionary'
+  },
+
+  seedDB: true
+};
