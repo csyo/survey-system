16a740ea6a6f4a66b055aaa5f7f6ff31fb411255
csyo <csyrick@icloud.com>
2015-06-03 19:57:57 +0800

    Minor refactoring before adding code

--- client/app/editor/upload/upload.controller.js
+++ client/app/editor/upload/upload.controller.js
@@ -2,30 +2,34 @@
 
 angular.module('surveyApp')
   .controller('UploadCtrl', function ($scope, Upload, ngDialog, surveydata) {
-    $scope.$watch('files', function () {
-      $scope.upload($scope.files);
-    });
+  var upload = this;
+  this.start = start;
+  this.done = done;
 
-    $scope.upload = function (files) {
-      if (files && files.length) {
-        for (var i = 0; i < files.length; i++) {
-          var file = files[i];
-          Upload.upload({
-            url: '/api/uploads',
-            fields: {'username': surveydata.getUserData().info.name},
-            file: file
-          }).progress(function (evt) {
-            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-            console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-          }).success(function (data, status, headers, config) {
-            console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
-          });
-        }
-      }
-    };
-
-    $scope.done = function () {
-      // TODO: add infos to surveydata
-      ngDialog.closeAll();
-    };
+  $scope.$watch('files', function () {
+    upload.start(upload.files);
   });
+
+  function start (files) {
+    if (files && files.length) {
+      for (var i = 0, j = files.length; i < j; i++) {
+        var file = files[i];
+        Upload.upload({
+          url: '/api/uploads',
+          fields: {'account': surveydata.getUserName().name},
+          file: file
+        }).progress(function (evt) {
+          var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+          console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+        }).success(function (data, status, headers, config) {
+          console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+        });
+      }
+    }
+  }
+
+  function done () {
+    // TODO: add infos to surveydata
+    ngDialog.closeAll();
+  };
+});
